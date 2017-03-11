b97b9d82e887f6686b2a90d6066e512f8c5ba71b
Nick Warwick <n_warwick@yahoo.ca>
2017-03-11 14:59:53 -0800

    Style map

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import Map, {GoogleApiWrapper, Marker} from 'google-maps-react';
+import Map, { GoogleApiWrapper, Marker } from 'google-maps-react';
 
 export class MapContainer extends React.Component {
   render() {
     if (!this.props.loaded) {
       return <div>Loading...</div>
     }
-    
+
+    const mapStyle = {
+      width: '50%',
+      height: '50%',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    };
+
     return (
-      <Map google={this.props.google}>
-      	<Marker
-      		onClick={() => alert('you clicked a marker!')}
-      		name={'location'}
-      	/>
+      <Map id="map" google={this.props.google} style={mapStyle} >
+        <Marker
+          onClick={() => alert('you clicked a marker!')}
+          name={'location'}
+        />
       </Map>
     )
   }
