36d80bdd698d3c5613ba5c20357a1200ace443e0
Nick Warwick <n_warwick@yahoo.ca>
2017-03-11 15:00:08 -0800

    Add header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import HeaderComponent from './components/HeaderComponent';
 import MapContainer from './components/MapContainer.js';
 
 class App extends Component {
   render() {
     return (
-      <MapContainer />
+      <div>
+        <HeaderComponent />
+        <MapContainer />
+      </div>
     );
   }
 }
---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HeaderComponent extends React.Component {
+  render() {
+    
+    return (
+        <div id="header">
+          <h1 id="main-title">Gather</h1>
+        </div>
+    );
+  }
+}
+
+export default HeaderComponent;
