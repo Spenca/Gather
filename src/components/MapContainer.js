5476b1c32feaed91ae4cb1b77f189724cd5c082c
mabsaleh <abousaleh.mohammed@gmail.com>
2017-03-11 14:29:18 -0800

    Add sample InfoWindow

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,21 +1,61 @@
 import React from 'react';
-import Map, {GoogleApiWrapper, Marker} from 'google-maps-react';
+import Map, {GoogleApiWrapper, Marker, InfoWindow} from 'google-maps-react';
 
 export class MapContainer extends React.Component {
-  render() {
-    if (!this.props.loaded) {
-      return <div>Loading...</div>
-    }
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			showingInfoWindow: false,
+			activeMarker: {},
+			selectedPlace: {},
+		}
+
+		this.onMapClick = this._onMapClick.bind(this);
+		this.onMarkerClick = this._onMarkerClick.bind(this);
+	}
+
+	_onMapClick(props, marker, e) {
+		if (this.state.showingInfoWindow) {
+      		this.setState({
+        		showingInfoWindow: false,
+        		activeMarker: null
+      		})
+    	}
+	}
+
+	_onMarkerClick(props, marker, e) {
+		this.setState({
+			selectedPlace: props,
+			activeMarker: marker,
+			showingInfoWindow: true
+		})
+	}
+
+	render() {
+    	if (!this.props.loaded) {
+      		return <div>Loading...</div>
+    	}
     
-    return (
-      <Map google={this.props.google}>
-      	<Marker
-      		onClick={() => alert('you clicked a marker!')}
-      		name={'location'}
-      	/>
-      </Map>
-    )
-  }
+	    return (
+	      <Map 
+	      	google={this.props.google}
+	      	onClick={this.onMapClick}>
+	      	<Marker
+	      		ref='testRef'
+	      		onClick={this.onMarkerClick}
+	      		name={'location'} />
+	      	<InfoWindow
+          		marker={this.state.activeMarker}
+          		visible={this.state.showingInfoWindow}>
+            	<div>
+              		<h1>{this.state.selectedPlace.name}</h1>
+            	</div>
+        	</InfoWindow>
+	      </Map>
+	    )
+  	}
 }
 
 export default GoogleApiWrapper({
