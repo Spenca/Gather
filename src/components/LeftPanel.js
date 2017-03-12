33f88c0ba20b1b0378902675f9439648c62fbba4
Nick Warwick <n_warwick@yahoo.ca>
2017-03-11 21:21:28 -0800

    Replace gathering with gatheringlist

--- src/components/LeftPanel.js
+++ src/components/LeftPanel.js
@@ -4,20 +4,16 @@ import SearchBar from './SearchBar';
 import GatheringList from './GatheringList';
 
 class LeftPanel extends React.Component {
+  
   render() {
     
     return (
         <div id="left-panel">
             <h1 id="panel-title">Event List</h1>
             <SearchBar />
-            <Gathering
-            	name={"CSC 330 study session"}
-            	address ={"Bay Center"}
-            	date={"March 12"}
-            	time={"7:00-9:00PM"}
-              description={"This is a description"}
+            <GatheringList 
+              onSelectGathering={this.props.onSelectGathering}
             />
-            <GatheringList />
         </div>
     );
   }
