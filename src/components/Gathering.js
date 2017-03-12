d7e13d81b5550e082d37f9f80b80a003080c0551
Nick Warwick <n_warwick@yahoo.ca>
2017-03-12 01:39:20 -0800

    Fix naming convention

--- src/components/Gathering.js
+++ src/components/Gathering.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
 class Gathering extends React.Component {
-	constructor(props) {
-    	super(props);
-    }
+	
 
   	render() {
 	    return (
 	    	<a type="button" href="#" className="list-group-item" onClick={() => this.props.onSelectGathering(this.props.gathering)}>
 		          <h4 className="list-group-item-heading">{this.props.gathering.title}</h4>
-		          <p className="list-group-item-text">{this.props.gathering.address}</p>
+		          <p className="list-group-item-text">{this.props.gathering.location}</p>
 		          <p className="list-group-item-text">{this.props.gathering.time} {this.props.gathering.date}</p>
 		    </a>
 	    );
