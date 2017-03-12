3a4bd3f1600c13042e6950fef45a73f98115e003
Nick Warwick <n_warwick@yahoo.ca>
2017-03-11 19:05:51 -0800

    Add wrapping div

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,9 +4,12 @@ class SearchBar extends React.Component {
     render() {
 
         return (
-            <div class="form-group">
-                <label class="control-label" for="focusedInput">Focused input</label>
-                <input class="form-control" id="focusedInput" type="text" value="This is focused..."/>
+            <div id="search-bar">
+                <div class="form-group">
+                    <label class="control-label" for="focusedInput">Focused input</label>
+                    <input class="form-control" id="focusedInput" type="text" value="This is focused..."/>
+
+                </div>
             </div>
         );
   }
