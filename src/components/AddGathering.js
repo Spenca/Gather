f89edb9eb415260d9ce1b219654877982970a010
Nick Warwick <n_warwick@yahoo.ca>
2017-03-11 22:59:52 -0800

    Add "add gathering" button

---
+++ src/components/AddGathering.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AddGathering extends React.Component {
+  
+  render() {
+    return (
+        <div id="add-gathering-button">
+            <a href="#" class="btn btn-lg">Add gathering</a>
+        </div>
+    );
+  }
+}
+
+export default AddGathering;
