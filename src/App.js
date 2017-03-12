9c171ec9ec00531fc17479310b3edcba7cfe92bf
Nick Warwick <n_warwick@yahoo.ca>
2017-03-11 18:31:37 -0800

    Rename component imports

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import HeaderComponent from './components/HeaderComponent';
-import MapContainer from './components/MapContainer.js';
+import Header from './components/Header';
+import LeftPanel from './components/LeftPanel';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <HeaderComponent />
-        <MapContainer />
+          <Header />
+          <LeftPanel />
       </div>
     );
   }
