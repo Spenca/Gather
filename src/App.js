7759d2c691f968e1dcf864214fac89d2e471ed2d
Nick Warwick <n_warwick@yahoo.ca>
2017-03-11 21:22:21 -0800

    Add main container

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import './App.css';
 
 import Header from './components/Header';
 import LeftPanel from './components/LeftPanel';
+import InfoPanel from './components/InfoPanel';
 
 class App extends Component {
+
+  updateInfoPanel(gathering) {
+     alert("gathering name: " + gathering.name);
+  }
+
+
+
   render() {
     return (
       <div>
           <Header />
-          <LeftPanel />
+          <div id="main-container">
+            <LeftPanel 
+              onSelectGathering={this.updateInfoPanel}
+            />
+            <InfoPanel 
+            />
+          </div>
       </div>
     );
   }
